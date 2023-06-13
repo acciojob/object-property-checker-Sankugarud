298e2c7ac470e0e8ca866f9257974e7641593098
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-13 20:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	sampleObject.map( (element) => {
-		if(element.key === key){
+	sampleObject.forEach( (element) => {
+		if(element.contains(key)){
 			return true;
-	
+		}else{
+			return false;
 		}
 	})
 }
+console.log(hasKey('red'));
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
 
