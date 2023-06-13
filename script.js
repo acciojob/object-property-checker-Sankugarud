7b65b809d5b534cd9fbf2006b4261f380b153f0f
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-13 20:41:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	sampleObject.map( (element => {
+	sampleObject.map( (element) => {
 		if(element.key === key){
 			return true;
+	
 		}
 	})
 }
